189dfddc43841b54041df6b62c2b38158c6b4e46
CarlosDanielGuagliardi <guagliardidaniel@gmail.com>
2023-02-26 23:24:54 -0300

    Primera escritura en el repositorio

---
+++ js/jswebcv-dos.js
@@ -0,0 +1,22 @@
+
+
+document.getElementById('edad').addEventListener('click', function(){
+    document.getElementById("demo").innerHTML = "Tengo 30 años";
+});
+
+document.getElementById('nacionalidad').addEventListener('click', function(){
+    document.getElementById("demo").innerHTML = "Argentina";
+});
+
+document.getElementById('estudios').addEventListener('click', function(){
+    document.getElementById("demo").innerHTML = "Secundario completo";
+});
+
+document.getElementById('experiencia').addEventListener('click', function(){
+    document.getElementById("demo2").innerHTML = "Tengo experiencia en: ";
+});
+
+document.getElementById('habilidades').addEventListener('click', function(){
+    document.getElementById("demo3").innerHTML = "Algunas de mis habilidades: ";
+});
+
